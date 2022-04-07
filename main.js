03c560211225e940f0cc9c2cb39049a559d432db
Angelina Blumenthal <angelina.blumenthal@gmail.com>
2022-04-07 14:37:16 +0200

    start project

---
+++ main.js
@@ -0,0 +1,25 @@
+let countDownDate = new Date("Apr 7, 2022 21:00:00").getTime();
+
+let x = setInterval(function () {
+  let now = new Date().getTime();
+  let distance = countDownDate - now;
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("countdown").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML =
+      "Ganz bald hoffentlich wieder";
+  }
+}, 1000);
+
+const delay = document.getElementById("delay");
+
+function myMessage() {
+  delay.innerHTML = "* Falls Lina pünktlich ist";
+}
+setTimeout(myMessage, 6000);
